0f41e9508b8bdf0f41d65f5cd044e7b4ed95de3c
floan <floan@usc.edu>
2020-06-27 18:37:15 +0500

    Added Comments

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
-const buttonClicked = (tab) => {
+const buttonClicked = (tab) => { //Sends a message to the content script to tell it to run
 	chrome.tabs.sendMessage(tab.id, "execute");
 }
 
-chrome.browserAction.onClicked.addListener(buttonClicked);
+chrome.browserAction.onClicked.addListener(buttonClicked); //Listener to detect when button is clicked
 
--- content.js
+++ content.js
@@ -1,8 +1,8 @@
 console.log("Chrome Extension Ready to Go!")
 
-let gradeArray = [];
+let gradeArray = []; //Initializing an array to store the grades
 
-const getGrades = () =>{
+const getGrades = () =>{ //Function to get grades from comments using the DOM
 	let ratingBox = document.getElementsByClassName("MetaItem__StyledMetaItem-y0ixml-0 bxqwYh");
 	gradeArray = [];
 	for(let i=0; i<ratingBox.length; i++){
@@ -15,9 +15,9 @@ const getGrades = () =>{
 }
 
 const gotMessage = (message, sender, sendResponse) => {
-	if(message === "execute"){
+	if(message === "execute"){ //Function is only called when the button is pressed
 		getGrades(); 
 	}
 }
 
-chrome.runtime.onMessage.addListener(gotMessage);
+chrome.runtime.onMessage.addListener(gotMessage); //Calls the gotMessage function when the listener detects a message
