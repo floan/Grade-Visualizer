7c3c37efa4917455153572a056889557ba603867
floan <floan@usc.edu>
2020-07-07 18:50:02 +0500

    Production Ready

--- content.js
+++ content.js
@@ -1,17 +1,17 @@
 let gradeArray = []; //Initializing an array to store the grades
 
 const getGrades = () =>{ //Function to get grades from comments using the DOM
-	let ratingBox = document.getElementsByClassName("MetaItem__StyledMetaItem-y0ixml-0 bxqwYh");
+	let ratingBox = document.getElementsByClassName('MetaItem__StyledMetaItem-y0ixml-0 bxqwYh');
 	gradeArray = [];
 	for(let i=0; i<ratingBox.length; i++){
-		if(ratingBox[i].textContent.includes("Grade")){
+		if(ratingBox[i].textContent.includes('Grade')){
 			gradeArray.push(ratingBox[i].children[0].textContent);
 		}
 	}
 }
 
 const gotMessage = (message, sender, sendResponse) => {
-	if(message === "execute"){ //Function is only called when the button is pressed
+	if(message === 'execute'){ //Function is only called when the button is pressed
 		getGrades(); 
 		chrome.runtime.sendMessage({text: "data", info: gradeArray}); //Sending the gathered info to the popup file
 	}
--- popup.js
+++ popup.js
@@ -1,33 +1,37 @@
-let makeChart = (AA, A, BBB, BB, B, CCC, CC, C) =>
+let makeChart = (AA, A, BBB, BB, B, CCC, CC, C, DDD, DD) =>
 {
     let ctx = document.getElementById('chart').getContext('2d');
 
     let myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: ['A+/A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-'],
+            labels: ['A+/A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D'],
             datasets: [{
                 label: '# of Votes',
-                data: [AA, A, BBB, BB, B, CCC, CC, C],
+                data: [AA, A, BBB, BB, B, CCC, CC, C, DDD, DD],
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                    'rgba(0, 50, 50, 0.2)',
-                    'rgba(0, 49, 82, 0.2)'
+                    '#66ff00',
+                    '#a6ff00',
+                    '#d1ff00',
+                    '#e6ff00',
+                    '#ffee00',
+                    '#ffd800',
+                    '#ffae00',
+                    '#ff9800',
+                    '#ff5800',
+                    '#ff1800'
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
-                    'rgba(0, 50, 50, 1)',
-                    'rgba(0, 49, 82, 1)'
+                    '#66ff00',
+                    '#a6ff00',
+                    '#d1ff00',
+                    '#e6ff00',
+                    '#ffee00',
+                    '#ffd800',
+                    '#ffae00',
+                    '#ff9800',
+                    '#ff5800',
+                    '#ff1800'
                 ],
                 borderWidth: 1
             }]
@@ -46,8 +50,8 @@ let makeChart = (AA, A, BBB, BB, B, CCC, CC, C) =>
 }
 
 const formatInfo = (info) => { //Function to parse the array and gather number of grades
-    let labels = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-'];
-    let results = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    let labels = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D'];
+    let results = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     for(let i=0; i<info.length; i++){
         for(let j=0; j<labels.length; j++){
@@ -59,7 +63,7 @@ const formatInfo = (info) => { //Function to parse the array and gather number o
     }
 
     makeChart(results[0]+results[1], results[2], results[3], results[4], results[5],
-                results[6], results[7], results[8], results[9]); //Calling make char function with grade #s as parameters
+                results[6], results[7], results[8], results[9], results[10], results[11]); //Calling make char function with grade #s as parameters
 
 }
 
@@ -73,7 +77,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
 const prompted = () => { //Sends a message to the content script to tell it to run
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, "execute");
+        chrome.tabs.sendMessage(tabs[0].id, 'execute');
     });
 }
 
